b5d8057372355eb3de9720c0c126b38fedad11f4
Ossama Chaib <ossama.chaib17@imperial.ac.uk>
2023-01-26 22:02:14 +0000

    First commit.

---
+++ lambda_handler.py
@@ -0,0 +1,5 @@
+# Using python, write a script that fetches data from 3 APIs, formats,
+# manipulates and saves the data to a local data store, then sends the final results to an endpoint.
+
+def lambda_handler():
+    return
